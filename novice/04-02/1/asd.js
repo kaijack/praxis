72fc8ea13952a9d019f000c95eb4539a89bf23dc
kaijack <jakkse7en@gmail.com>
2019-07-09 04:33:21 -0400

    update 9 july

---
+++ novice/04-02/1/asd.js
@@ -0,0 +1,15 @@
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [{
+                text: 'Learn JavaScript'
+            },
+            {
+                text: 'Learn Vue'
+            },
+            {
+                text: 'Build something awesome'
+            }
+        ]
+    }
+})
---
+++ novice/04-03/2/myscript.js
@@ -0,0 +1,19 @@
+  #
+  demo {
+    font - family: 'Helvetica', Arial, sans - serif;
+  }
+
+  a {
+    text - decoration: none;
+    color: #f66;
+  }
+
+  li {
+    line - height: 1.5 em;
+    margin - bottom: 20 px;
+  }
+
+  .author,
+  .date {
+    font - weight: bold;
+  }
